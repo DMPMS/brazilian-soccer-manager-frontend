f5f0bb24a4257e585fc65bbd06d976b9603b3186
Davi Monteiro <monteiro_davi@yahoo.com>
2024-04-16 19:20:20 -0300

    Criando página not found (404) e removendo função verifyToken na firstScreen

---
+++ src/modules/firstScreen/screens/PageNotFound.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../../shared/components/buttons/button/Button';
+import { LoginRoutesEnum } from '../../login/routes';
+import {
+  ContainerError,
+  ContainerPageNotFound,
+  DeflatedBallImage,
+  DescriptionError,
+  TitleError,
+} from '../styles/pageNotFound.styles';
+
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  const handleOnClickButton = () => {
+    navigate(LoginRoutesEnum.LOGIN);
+  };
+  return (
+    <ContainerPageNotFound>
+      <ContainerError>
+        <DeflatedBallImage src="../../../../public/deflatedBall.png" />
+        <TitleError>Erro 404</TitleError>
+        <DescriptionError>Ops! Parece que essa página está fora de jogo.</DescriptionError>
+        <Button onClick={handleOnClickButton} margin="32px 0px 0px 0px" type="primary">
+          Página de Login
+        </Button>
+      </ContainerError>
+    </ContainerPageNotFound>
+  );
+};
+
+export default PageNotFound;
